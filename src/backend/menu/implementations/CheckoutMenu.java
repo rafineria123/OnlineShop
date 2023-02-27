35888b123d2d5c61a080113ebf51bb7877d5c006
rafineria123 <37177277+rafineria123@users.noreply.github.com>
2023-02-27 14:48:14 +0100

    CheckoutMenu class implemented

--- src/backend/menu/implementations/CheckoutMenu.java
+++ src/backend/menu/implementations/CheckoutMenu.java
@@ -1,9 +1,14 @@
 package backend.menu.implementations;
 
 import backend.config.ApplicationContext;
+import backend.entities.Order;
+import backend.entities.Product;
+import backend.entities.implementations.DefaultOrder;
 import backend.menu.Menu;
 import backend.services.OrderManagementService;
 import backend.services.implementations.DefaultOrderManagementService;
+
+import java.util.Scanner;
 
 public class CheckoutMenu implements Menu {
 
@@ -17,12 +22,39 @@ public class CheckoutMenu implements Menu {
 
     @Override
     public void start() {
-        // <write your code here>
+        printMenuHeader();
+        if (context.getLoggedInUser() == null) {
+            System.out.println("You are not logged in. Please, sign in or create new account");
+            context.getMainMenu().start();
+        }else if(context.getSessionCart().isEmpty()){
+            System.out.println("Your cart is empty. Please, add product to cart first and then proceed with checkout");
+            new ProductCatalogMenu().start();
+        }else {
+            while(true) {
+                System.out.println("Enter your credit card number without spaces and press enter if you confirm purchase");
+                Scanner scanner = new Scanner(System.in);
+                String input = scanner.nextLine();
+                Order order = new DefaultOrder();
+                if (!order.isCreditCardNumberValid(input)) {
+                    System.out.println("You entered invalid credit card number. Valid credit card should contain 16 digits. Please, try one more time.");
+                    continue;
+                }
+                order.setCreditCardNumber(input);
+                order.setProducts(context.getSessionCart().getProducts().toArray(Product[]::new));
+                order.setCustomerId(context.getLoggedInUser().getId());
+                orderManagementService.addOrder(order);
+                System.out.println("Thanks a lot for your purchase. Details about order delivery are sent to your email.");
+                context.getSessionCart().clear();
+                context.getMainMenu().start();
+            }
+
+        }
     }
 
     @Override
     public void printMenuHeader() {
-        // <write your code here>
+        System.out.println("*** CHECKOUT ***");
     }
+
 
 }
